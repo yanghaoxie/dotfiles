1c03ebd28cd05d25c4e649df6958eaf28aaf9925
Yanghao Xie <yhaoxie@gmail.com>
2018-07-24 19:37:43 +0800

    change name

--- spacemacs/.emacs.d/private/init-pdf-tools.el
+++
@@ -1,6 +0,0 @@
-(spacemacs|use-package-add-hook pdf-tools
-  :post-config
-  (evilified-state-evilify pdf-view-mode pdf-view-mode-map
-    "j" '(lambda() (interactive) (pdf-view-next-line-or-next-page 35))))
-
-(provide 'init-pdf-tools)
