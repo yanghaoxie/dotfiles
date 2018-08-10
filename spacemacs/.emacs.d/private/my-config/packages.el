67a41b1bfb188b9172a1b6da2435ae21ad1a470e
Yanghao Xie <yhaoxie@gmail.com>
2018-08-10 20:53:06 +0800

    set org-agenda-files as all org files in ~/Dropbox/document/org recursively

--- spacemacs/.emacs.d/private/my-config/packages.el
+++ spacemacs/.emacs.d/private/my-config/packages.el
@@ -133,6 +133,12 @@ e       `./local/PACKAGE/PACKAGE.el'
     ;; (add-hook 'org-mode-hook 'turn-on-cdlatex)
     (require 'ox-bibtex)
     (setq org-directory "~/Dropbox/document/org")
+    (setq org-agenda-files (apply 'append
+			                            (mapcar
+			                             (lambda (directory)
+				                             (directory-files-recursively
+				                              directory org-agenda-file-regexp))
+			                             '("~/Dropbox/document/org/" ))))
     (setq org-latex-pdf-process
           '(
             "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
