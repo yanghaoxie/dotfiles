c555a732c6bc27263cfc2f738e25f4c771436604
Yanghao Xie <yhaoxie@gmail.com>
2018-10-21 08:28:53 +0800

    use left ctrl as capslock and left ctrl

--- xkeysnail/.config/xkeysnail/xkeysnail-config.py
+++ xkeysnail/.config/xkeysnail/xkeysnail-config.py
@@ -24,6 +24,7 @@ from xkeysnail.transform import *
 define_multipurpose_modmap({
     Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL],
     Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
+    Key.LEFT_CTRL: [Key.CAPSLOCK, Key.LEFT_CTRL],
     # Key.LEFT_CTRL: [Key.CAPSLOCK, Key.LEFT_CTRL],
     # Key.SPACE: [Key.SPACE, Key.LEFT_META],
     })
