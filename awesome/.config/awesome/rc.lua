7467f4c98e5196cacfcd601648ff0f64e9cf9c68
Yanghao Xie <yhaoxie@gmail.com>
2018-12-15 20:42:59 +0800

    disable key binding for show calendar

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -419,8 +419,8 @@ globalkeys = my_table.join(
               {description = "dropdown application", group = "launcher"}),
 
     -- Widgets popups
-    awful.key({ altkey, }, "c", function () if beautiful.cal then beautiful.cal.show(7) end end,
-              {description = "show calendar", group = "widgets"}),
+    -- awful.key({ altkey, }, "c", function () if beautiful.cal then beautiful.cal.show(7) end end,
+    --           {description = "show calendar", group = "widgets"}),
     awful.key({ altkey, }, "h", function () if beautiful.fs then beautiful.fs.show(7) end end,
               {description = "show filesystem", group = "widgets"}),
     awful.key({ altkey, }, "w", function () if beautiful.weather then beautiful.weather.show(7) end end,
