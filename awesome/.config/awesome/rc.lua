6efa1aaa473b4a999a0de4ad7e2fd9b99eda12c3
Yanghao Xie <yhaoxie@gmail.com>
2018-12-15 19:56:53 +0800

    use theme No. 7

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -85,7 +85,7 @@ local themes = {
     "vertex",          -- 10
 }
 
-local chosen_theme = themes[6]
+local chosen_theme = themes[7]
 local modkey       = "Mod4"
 local altkey       = "Mod1"
 local terminal     = "urxvtc"
