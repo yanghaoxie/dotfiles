8cf50acc7c166b08e1d430a573cdeac1cd84aff0
Yanghao Xie <yhaoxie@gmail.com>
2018-12-15 19:59:22 +0800

    use modkey+q to close client

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -530,7 +530,7 @@ clientkeys = my_table.join(
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
+    awful.key({ modkey,           }, "q",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
