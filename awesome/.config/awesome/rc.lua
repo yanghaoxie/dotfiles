4d6741fb273c70a46922351fae2e5aeaea01ab91
Yanghao Xie <yhaoxie@gmail.com>
2018-12-15 19:58:40 +0800

    use alt+tab to focus previous client

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -316,7 +316,7 @@ globalkeys = my_table.join(
               {description = "focus the previous screen", group = "screen"}),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
-    awful.key({ modkey,           }, "Tab",
+    awful.key({ altkey,           }, "Tab",
         function ()
             awful.client.focus.history.previous()
             if client.focus then
