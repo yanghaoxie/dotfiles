bab112eba8291ab511ffd440b653d1a47c7d6f41
Yanghao Xie <yhaoxie@gmail.com>
2018-12-16 14:56:47 +0800

    use modkey+f to toggle floating

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -557,7 +557,7 @@ clientkeys = my_table.join(
         {description = "toggle fullscreen", group = "client"}),
     awful.key({ modkey,           }, "q",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
+    awful.key({ modkey, }, "f",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
               {description = "move to master", group = "client"}),
