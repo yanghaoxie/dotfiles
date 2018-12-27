bbc8c3c6201e6b38798cd7c55f23893e38f2159f
Yanghao Xie <yhaoxie@gmail.com>
2018-12-27 15:48:14 +0800

    use gnome-session-quit when quit awesome

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1,3 +1,13 @@
+-- Override awesome.quit when we're using GNOME
+_awesome_quit = awesome.quit
+awesome.quit = function()
+    if os.getenv("DESKTOP_SESSION") == "awesome-gnome" then
+       os.execute("/usr/bin/gnome-session-quit") -- for Ubuntu 14.04
+       os.execute("pkill -9 gnome-session") -- I use this on Ubuntu 16.04
+    else
+    _awesome_quit()
+    end
+end
 --[[
 
      Awesome WM configuration template
