e4a138a2761b8b07752260c35118e6ae51d13611
Yanghao Xie <yhaoxie@gmail.com>
2019-01-09 16:35:36 +0800

    rules for firefox

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -689,9 +689,10 @@ awful.rules.rules = {
     -- { rule_any = { type = { "dialog", "normal" } },
     --   properties = { titlebars_enabled = false} },
 
-    -- Set Firefox to always map on the first tag on screen 1.
     { rule = { class = "Firefox" },
-      properties = { screen = 1, tag = awful.util.tagnames[1] } },
+      properties = { titlebars_enabled = false,
+		     floating = false}},
+
 
     { rule = { class = "Gimp", role = "gimp-image-window" },
           properties = { maximized = true } },
