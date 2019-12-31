dcad434430b45060167e02170b0f315dce0c726b
Yanghao Xie <yhaoxie@gmail.com>
2019-12-31 10:01:14 +0800

    do not need to start gsd-xsetting manually, since it is started by WeChat script

--- bin/bin/startup_script.sh
+++ bin/bin/startup_script.sh
@@ -12,7 +12,4 @@ unclutter --timeout 5 &
 # start xkeysnail
 sudo /usr/bin/xkeysnail --watch -q /home/yhxie/.config/xkeysnail/xkeysnail-config.py >/dev/null 2>&1 &
 
-# start gsd-xsettings which is needed TIM and wechat
-/usr/lib/gsd-xsettings &
-
 V2RAY_LOCATION_ASSET=/etc/v2ray v2ray --config /etc/v2ray/config-1.json &
