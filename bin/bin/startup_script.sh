3798947e7ebca6d152bc0df7a1bd844265978619
Yanghao Xie <yhaoxie@gmail.com>
2019-12-31 10:01:43 +0800

    use systemd to start v2ray

--- bin/bin/startup_script.sh
+++ bin/bin/startup_script.sh
@@ -11,5 +11,3 @@ unclutter --timeout 5 &
 
 # start xkeysnail
 sudo /usr/bin/xkeysnail --watch -q /home/yhxie/.config/xkeysnail/xkeysnail-config.py >/dev/null 2>&1 &
-
-V2RAY_LOCATION_ASSET=/etc/v2ray v2ray --config /etc/v2ray/config-1.json &
